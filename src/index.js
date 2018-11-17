103efd08922f81687d56b66ab8acc27f58172919
Dawid Kozak <dawid.kozak@netcentric.biz>
2018-11-17 12:32:54 +0100

    Ladowanie obrazkow

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import "./style.css";
+import Placeholder from "./placeholder.png";
 
 function component() {
   let element = document.createElement("div");
   element.innerHTML = "Hello webpack";
   element.classList.add("hello");
+
+  let myIcon = new Image();
+  myIcon.src = Placeholder;
+  element.appendChild(myIcon);
+
   return element;
 }
 
